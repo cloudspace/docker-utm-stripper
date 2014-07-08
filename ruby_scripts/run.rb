576b36ef0883e3c9c199e54eabb5387035acc0ee
Michael Orr <michael@cloudspace.com>
2014-07-08 10:04:31 -0400

    handle url's without a query string

--- ruby_scripts/run.rb
+++ ruby_scripts/run.rb
@@ -1,6 +1,8 @@
 require 'uri'
 
 uri = URI.parse(ARGV[0])
-clean_key_vals = URI.decode_www_form(uri.query).reject{|k, _| k.start_with?('utm_')}
-uri.query = URI.encode_www_form(clean_key_vals)
+if uri.query
+  clean_key_vals = URI.decode_www_form(uri.query).reject{|k, _| k.start_with?('utm_')}
+  uri.query = URI.encode_www_form(clean_key_vals)
+end
 puts uri.to_s
