cfa997f93f6a86d662e4b3fe6457500a18793491
Michael Orr <michael@cloudspace.com>
2014-07-07 22:23:21 -0400

    faster removal using gsub instead of using the uri library

--- ruby_scripts/run.rb
+++ ruby_scripts/run.rb
@@ -1,6 +1,1 @@
-require 'uri'
-
-uri = URI.parse(ARGV[0])
-clean_key_vals = URI.decode_www_form(uri.query).reject{|k, _| k.start_with?('utm_')}
-uri.query = URI.encode_www_form(clean_key_vals)
-puts uri.to_s
+puts ARGV[0].gsub(/utm_.+?(&|$)/, '').gsub(/\?$/,'')
