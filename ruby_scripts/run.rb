a6006480ef294e69d17b86f29f771c58190b4e23
Michael Orr <michael@cloudspace.com>
2014-07-08 12:32:57 -0400

    remove extra decode and extra puts

--- ruby_scripts/run.rb
+++ ruby_scripts/run.rb
@@ -1,19 +1,7 @@
 require 'uri'
-puts "1"
-puts ARGV[0]
-uri = URI.parse(URI.decode(ARGV[0]))
-puts "2"
-puts uri
+uri = URI.parse(ARGV[0])
 if uri.query
-  puts "3"
-  puts uri.query
   clean_key_vals = URI.decode_www_form(uri.query).reject{|k, _| k.start_with?('utm_')}
-  puts "4"
-  puts clean_key_vals
   uri.query = URI.encode_www_form(clean_key_vals)
-  puts "5"
-  puts uri.query
-  puts "6"
-  puts uri
 end
 puts uri.to_s
