01a35638a5996861b2f3245d2fdaa3717dc8457c
Michael Orr <michael@cloudspace.com>
2014-07-08 14:17:27 -0400

    having a decode when properly encoded seems to work

--- ruby_scripts/run.rb
+++ ruby_scripts/run.rb
@@ -1,7 +1,5 @@
 require 'uri'
-uris = URI.extract(ARGV[0])
-uri = URI.decode(uris[0])
-uri = URI.parse(uri)
+uri = URI.parse(URI.decode(ARGV[0]))
 if uri.query
   clean_key_vals = URI.decode_www_form(uri.query).reject{|k, _| k.start_with?('utm_')}
   uri.query = URI.encode_www_form(clean_key_vals)
