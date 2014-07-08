46e8579c69d8e942a0e4b0db2719f36fedb3c1b3
Michael Orr <michael@cloudspace.com>
2014-07-08 11:13:24 -0400

    decode the encoded uri before parsing

--- ruby_scripts/run.rb
+++ ruby_scripts/run.rb
@@ -1,6 +1,6 @@
 require 'uri'
 
-uri = URI.parse(ARGV[0])
+uri = URI.parse(URI.decode(ARGV[0]))
 if uri.query
   clean_key_vals = URI.decode_www_form(uri.query).reject{|k, _| k.start_with?('utm_')}
   uri.query = URI.encode_www_form(clean_key_vals)
