76c36a0d3e1e83d29ee8ec38851eab8e05aee6bc
Michael Orr <michael@cloudspace.com>
2014-07-08 12:51:47 -0400

    trying some extract and decode

--- ruby_scripts/run.rb
+++ ruby_scripts/run.rb
@@ -1,5 +1,7 @@
 require 'uri'
-uri = URI.parse(ARGV[0])
+uris = URI.extract(ARGV[0])
+uri = URI.decode(uris[0])
+uri = URI.parse(uri)
 if uri.query
   clean_key_vals = URI.decode_www_form(uri.query).reject{|k, _| k.start_with?('utm_')}
   uri.query = URI.encode_www_form(clean_key_vals)
