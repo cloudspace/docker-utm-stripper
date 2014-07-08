018a295021564a6d32a30233d9c54421143f98d5
Michael Orr <michael@cloudspace.com>
2014-07-07 23:29:28 -0400

    going back to using the uri parsing library
    regexes that i've used fail in edge cases (like a variable containing an html encoded entity that starts with an & or are too greedy and eliminate good parameters in the middle of the parameter string

--- ruby_scripts/run.rb
+++ ruby_scripts/run.rb
@@ -1,1 +1,6 @@
-puts ARGV[0].gsub(/utm_.+?(&|$)/, '').gsub(/\?$/,'')
+require 'uri'
+
+uri = URI.parse(ARGV[0])
+clean_key_vals = URI.decode_www_form(uri.query).reject{|k, _| k.start_with?('utm_')}
+uri.query = URI.encode_www_form(clean_key_vals)
+puts uri.to_s
