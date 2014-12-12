81fccc8238256e2883aa6368bffdac829fa8141b
Michael Orr <michael@cloudspace.com>
2014-12-12 15:25:17 -0500

    output in json format

--- ruby_scripts/run.rb
+++ ruby_scripts/run.rb
@@ -4,4 +4,4 @@ if uri.query
   clean_key_vals = URI.decode_www_form(uri.query).reject{|k, _| k.start_with?('utm_')}
   uri.query = URI.encode_www_form(clean_key_vals)
 end
-puts uri.to_s
+puts "{\"url\": \"#{uri.to_s}\"}"
